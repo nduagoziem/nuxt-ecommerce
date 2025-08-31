c052a35d755548983bb8ec35e890f90deaa4dcb9
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2025-08-31 11:08:14 +0100

    added middleware

---
+++ middleware/guest.js
@@ -0,0 +1,14 @@
+import {
+  abortNavigation,
+  defineNuxtRouteMiddleware,
+} from "nuxt/app";
+import { useAuthStore } from "../stores/auth";
+
+export default defineNuxtRouteMiddleware(async (to) => {
+  const { isAuthenticated } = useAuthStore();
+  // if (import.meta.server) return;
+  if (isAuthenticated.customer != null) {
+    return abortNavigation(), to.path = "/" ;
+  } 
+  return;
+});
