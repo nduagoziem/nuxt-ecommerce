e908b4f5cc44218a4eb90dcf35ff547805457a07
nduagoziem <162841062+nduagoziem@users.noreply.github.com>
2025-10-09 12:04:29 +0100

    fix: changed comments and renamed a file

--- stores/auth.js
+++ stores/auth.js
@@ -4,7 +4,7 @@ import { reactive } from "vue";
 /**
  * The sole purpose of this store is for authentication
  *  Do not use it for any other logic apart from this.
- * The value of this store should only be set/changed in init-auth.global.js file
+ * The value of this store should only be set/changed in init-auth.js file
  */
 export const useAuthStore = defineStore("auth", () => {
   const isAuthenticated = reactive({ customer: null });
--- stores/customerInfo.js
+++ stores/customerInfo.js
@@ -4,7 +4,7 @@ import { reactive} from "vue";
 /**
  * The sole purpose of this store is for retrieving the customers' info
  *  Do not use it for any other logic apart from this.
- * The value of this store should only be set/changed in init-auth.global.js file
+ * The value of this store should only be set/changed in init-auth.js file
  */
 export const useCustomerInfoStore = defineStore("customerInfo", () => {
   const customerInfo = reactive({
